1f122812b6cac8c0f9ad14420691884f66222716
J4P032 <J4p032@gmail.com>
2024-12-23 23:44:21 +0100

    Referencias

---
+++ 14.Referencias/_DeclararReferencia/main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+/*Hay que declarar una variable referencia de la variable dada value1.
+La variable referencia se llamará ref_value1.
+No se puede modificar el nombre de la funcion que dan */
+
+void refs(){
+	int value{33};
+	int& ref_value1 = value1;
+	std::cout << value1 << ref_value1;
+}
+
+int main(void)
+{
+	refs();
+	return (0);
+}
